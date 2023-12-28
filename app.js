fc3881a202c381cff013d6be6b9e989fcb17f490
Gonza <Gonza963p@gmail.com>
2023-12-28 00:34:57 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const readlineSync = require('readline-sync');
+const { generarNumeroAleatorio, verificarAdivinanza} = require('./adivinanzas');
+
+const obtenerNumeroUsuario = () => {
+    return readlineSync.question('Ingresar un numero: ');
+
+};
+
+const juegoAdivinanza = () => {
+    const numeroSecreto = generarNumeroAleatorio();
+    let numeroAdivinado = 0;
+
+    console.log('¡Bienvenido a Adivinar el numero secreto!');
+    console.log('Intenta Adivinar el numero del 1 al 100. \n');
+
+    while (numeroAdivinado !== numeroSecreto) {
+        
+        const numeroSecreto = generarNumeroAleatorio();
+        
+        numeroAdivinado = obtenerNumeroUsuario(); 
+
+        verificarAdivinanza(numeroSecreto,numeroAdivinado);
+    }
+
+};
+
+juegoAdivinanza();
